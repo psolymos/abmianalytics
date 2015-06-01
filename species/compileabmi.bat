4a68a755012414deb8f771c8cb8567b6bc1c07c2
Peter Solymos <psolymos@gmail.com>
2015-06-01 13:59:40 -0600

    bat file updated if/else

--- species/compileabmi.bat
+++ species/compileabmi.bat
@@ -2,51 +2,86 @@
 @echo off
 :: output folder
 set out=y:\Oracle_access_2015\out\
+:: switch on/off
+set TEST=0
+set MASTERLIST=0
+set BIRDS=0
+set VPLANTS=0
+set MITES=0
+set MOSSES=0
+set LICHENS=0
+set MAMMALS=1
+set AQPLANTS=0
+set AQINVERTS=0
+set SUMMARY=0
 
-:: test
-:: echo Running test (hit OK to read/write exception error if happens)
-:: R CMD BATCH --vanilla "00test.R" "%out%OUT_test.log"
+:TEST_SUB
+IF %TEST%==1 (
+echo Running test (hit OK to read/write exception error if happens)
+R CMD BATCH --vanilla "00test.R" "%out%OUT_test.log"
+)
 
-:: MasterList
-:: echo Compiling master list for sites
-:: R CMD BATCH --vanilla "00masterlist.R" "%out%00masterlist.log"
+:MASTERLIST_SUB
+IF %MASTERLIST%==1 (
+echo Compiling master list for sites
+R CMD BATCH --vanilla "00masterlist.R" "%out%00masterlist.log"
+)
 
-:: Birds
+:BIRDS_SUB
+IF %BIRDS%==1 (
 echo Compiling species data for terrestrial birds
 R CMD BATCH --vanilla "birds.R" "%out%OUT_birds.log"
+)
 
-:: VPlants
+:VPLANTS_SUB
+IF %VPLANTS%==1 (
 echo Compiling species data for terrestrial vascular plants
 R CMD BATCH --vanilla "vplants.R" "%out%OUT_vplants.log"
+)
 
-:: Mites
+:MITES_SUB
+IF %MITES%==1 (
 echo Compiling species data for terrestrial mites
 R CMD BATCH --vanilla "mites.R" "%out%OUT_mites.log"
+)
 
-:: Mosses
+:MOSSES_SUB
+IF %MOSSES%==1 (
 echo Compiling species data for terrestrial mosses
 R CMD BATCH --vanilla "mosses.R" "%out%OUT_mosses.log"
+)
 
-:: Lichens
+:LICHENS_SUB
+IF %LICHENS%==1 (
 echo Compiling species data for terrestrial lichens
 R CMD BATCH --vanilla "lichens.R" "%out%OUT_lichens.log"
+)
 
-:: Mammals
-:: echo Compiling species data for terrestrial mammals
-:: R CMD BATCH --vanilla "c:\Users\Peter\repos\abmianalytics\species\R\mammals.R" "%out%OUT_mammals.log"
+:MAMMALS_SUB
+IF %MAMMALS%==1 (
+echo Compiling species data for terrestrial mammals
+R CMD BATCH --vanilla "mammals.R" "%out%OUT_mammals.log"
+)
 
-:: AqVPlants
-::echo Compiling species data for aquatic vascular plants
-::R CMD BATCH --vanilla "c:\Users\Peter\repos\abmianalytics\species\R\aqplants.R" "%out%OUT_aqplants.log"
+:AQPLANTS_SUB
+IF %AQPLANTS%==1 (
+echo Compiling species data for aquatic vascular plants
+R CMD BATCH --vanilla "aqplants.R" "%out%OUT_aqplants.log"
+)
 
-:: AqInverts
-::echo Compiling species data for aquatic invertebrates
-::R CMD BATCH --vanilla "c:\Users\Peter\repos\abmianalytics\species\R\aqinverts.R" "%out%OUT_aqinverts.log"
+:AQINVERTS_SUB
+IF %AQINVERTS%==1 (
+echo Compiling species data for aquatic invertebrates
+R CMD BATCH --vanilla "aqinverts.R" "%out%OUT_aqinverts.log"
+)
 
-:: Summary
-::echo Running summaries
-::R CMD BATCH --vanilla "c:\Users\Peter\repos\abmianalytics\species\R\00summary.R" "%out%00summary.log"
+:SUMMARY_SUB
+IF %SUMMARY%==1 (
+echo Running summaries
+R CMD BATCH --vanilla "00summary.R" "%out%00summary.log"
+)
 
+:END
 :: erase output folder variable
 set out=
 :: Exit
