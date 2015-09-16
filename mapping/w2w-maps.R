0c5b7e7f32bf22de971cf41c035c3cff9679ec86
Peter Solymos <psolymos@gmail.com>
2015-09-15 23:38:34 -0600

    thumbs

--- mapping/w2w-maps.R
+++ mapping/w2w-maps.R
@@ -78,15 +78,19 @@ hf1 <- as.matrix(groupSums(hf1, 2, hft$HF))
 #    scales=list(draw=FALSE), contour=FALSE, main="Human Footprint Types")
 #dev.off()
 
-pdf(file.path(ROOT, VER, "out", "figs", "hf", "hf-1file.pdf"),
-    width=8, height=12, onefile=TRUE)
+#pdf(file.path(ROOT, VER, "out", "figs", "hf", "hf-1file.pdf"),
+#    width=8, height=12, onefile=TRUE)
 for (i in 1:ncol(hf2)) {
+    png(file.path(ROOT, VER, "out", "figs", "hf", 
+        paste0(colnames(hf2)[i], ".png")),
+        width=600, height=1000)
     cat(i, "/", ncol(hf2), "\n");flush.console()
     x <- 100 * hf2[,i]
     map_fun(x, q=0.999, main=colnames(hf2)[i], 
         colScale="hf", maskRockies=FALSE)
+    dev.off()
 }
-dev.off()
+#dev.off()
 
 ## maps: soils
 
@@ -116,14 +120,18 @@ levels(r_overlay) <- rat
 
 source("~/repos/abmianalytics/R/maps_functions.R")
 
-pdf(file.path(ROOT, VER, "out", "figs", "soil", "soil-1file.pdf"),
-    width=8, height=12, onefile=TRUE)
+#pdf(file.path(ROOT, VER, "out", "figs", "soil", "soil-1file.pdf"),
+#    width=8, height=12, onefile=TRUE)
 for (i in 1:ncol(soil1)) {
+    png(file.path(ROOT, VER, "out", "figs", "soil", 
+        paste0(colnames(soil1)[i], ".png")),
+        width=600, height=1000)
     cat(i, "/", ncol(soil1), "\n");flush.console()
     map_fun(100*soil1[,i], q=0.999, main=colnames(soil1)[i], 
         colScale="soil", maskRockies=TRUE, plotWater=TRUE)
+    dev.off()
 }
-dev.off()
+#dev.off()
 
 ## maps: veg+age
 
@@ -142,9 +150,13 @@ vegAll <- cbind(veg4, veg2)
 pdf(file.path(ROOT, VER, "out", "figs", "veg", "veg-1file.pdf"),
     width=8, height=12, onefile=TRUE)
 for (i in 1:ncol(vegAll)) {
+    png(file.path(ROOT, VER, "out", "figs", "veg", 
+        paste0(colnames(vegAll)[i], ".png")),
+        width=600, height=1000)
     cat(i, "/", ncol(vegAll), colnames(vegAll)[i], "\n");flush.console()
     map_fun(100*vegAll[,i], q=0.999, main=colnames(vegAll)[i], 
         colScale="terrain", maskRockies=FALSE, plotWater=TRUE)
+    dev.off()
 }
 dev.off()
 
@@ -152,11 +164,79 @@ climAll <- kgrid[,c("AHM","PET","FFP","MAP","MAT","MCMT","MWMT")]
 pdf(file.path(ROOT, VER, "out", "figs", "clim", "clim-1file.pdf"),
     width=8, height=12, onefile=TRUE)
 for (i in 1:ncol(climAll)) {
+    png(file.path(ROOT, VER, "out", "figs", "clim", 
+        paste0(colnames(climAll)[i], ".png")),
+        width=600, height=1000)
     cat(i, "/", ncol(climAll), colnames(climAll)[i], "\n");flush.console()
     map_fun(climAll[,i], q=0.999, main=colnames(climAll)[i], 
         colScale="terrain", maskRockies=FALSE, plotWater=TRUE)
+    dev.off()
 }
 dev.off()
+
+## topo
+
+r <- raster("c:/p/AB_data_v2015/out/figs/topo/aggregates.asc")
+png(file.path(ROOT, VER, "out", "figs", "topo", "aggregates.png"), width=600, height=1000)
+plot(r, axes=FALSE, box=FALSE, legend=FALSE, main="Aggregate presence", maxpixels=10^6, interpolate=FALSE)
+dev.off()
+
+r <- raster("c:/p/AB_data_v2015/out/figs/topo/cti.asc")
+png(file.path(ROOT, VER, "out", "figs", "topo", "cti.png"), width=600, height=1000)
+plot(r, axes=FALSE, box=FALSE, legend=TRUE, main="Compound topographic index (wetness)", maxpixels=10^6, interpolate=FALSE)
+dev.off()
+
+r <- raster("c:/p/AB_data_v2015/out/figs/topo/dunes.asc")
+png(file.path(ROOT, VER, "out", "figs", "topo", "dunes.png"), width=600, height=1000)
+plot(r, axes=FALSE, box=FALSE, legend=FALSE, main="Dune presence", maxpixels=10^6, interpolate=FALSE)
+dev.off()
+
+r <- as.factor(raster("c:/p/AB_data_v2015/out/figs/topo/geol_bedcode.asc"))
+png(file.path(ROOT, VER, "out", "figs", "topo", "geol-bedcode.png"), width=600, height=1000)
+plot(r, axes=FALSE, box=FALSE, legend=FALSE, main="Bedrock geology (origin)", maxpixels=10^6, interpolate=FALSE)
+dev.off()
+
+r <- as.factor(raster("c:/p/AB_data_v2015/out/figs/topo/geol_surf.asc"))
+png(file.path(ROOT, VER, "out", "figs", "topo", "geol-surf.png"), width=600, height=1000)
+plot(r, axes=FALSE, box=FALSE, legend=FALSE, main="Surficial geology (parent material)", maxpixels=10^6, interpolate=FALSE)
+dev.off()
+
+r <- raster("c:/p/AB_data_v2015/out/figs/topo/slope.asc")
+png(file.path(ROOT, VER, "out", "figs", "topo", "slope.png"), width=600, height=1000)
+plot(r, axes=FALSE, box=FALSE, legend=TRUE, main="Slope", maxpixels=10^6, interpolate=FALSE)
+dev.off()
+
+r <- raster("c:/p/AB_data_v2015/out/figs/topo/slpasp.asc")
+png(file.path(ROOT, VER, "out", "figs", "topo", "slpasp.png"), width=600, height=1000)
+plot(r, axes=FALSE, box=FALSE, legend=TRUE, main="Slope / aspect solar radiation index", maxpixels=10^6, interpolate=FALSE)
+dev.off()
+
+r <- raster("c:/p/AB_data_v2015/out/figs/topo/spi.asc")
+png(file.path(ROOT, VER, "out", "figs", "topo", "spi.png"), width=600, height=1000)
+plot(r, axes=FALSE, box=FALSE, legend=TRUE, main="SPI", maxpixels=10^6, interpolate=FALSE)
+dev.off()
+
+r <- raster("c:/p/AB_data_v2015/out/figs/topo/tpi2km.asc")
+png(file.path(ROOT, VER, "out", "figs", "topo", "tpi2km.png"), width=600, height=1000)
+plot(r, axes=FALSE, box=FALSE, legend=TRUE, main="Topographic position index (2-km radius)", maxpixels=10^6, interpolate=FALSE)
+dev.off()
+
+r <- raster("c:/p/AB_data_v2015/out/figs/topo/tri.asc")
+png(file.path(ROOT, VER, "out", "figs", "topo", "tri.png"), width=600, height=1000)
+plot(r, axes=FALSE, box=FALSE, legend=TRUE, main="Topographic ruggedness index", maxpixels=10^6, interpolate=FALSE)
+dev.off()
+
+r <- raster("c:/p/AB_data_v2015/out/figs/topo/vrm11x11.asc")
+png(file.path(ROOT, VER, "out", "figs", "topo", "vrm11x11.png"), width=600, height=1000)
+plot(r, axes=FALSE, box=FALSE, legend=TRUE, main="Vector ruggedness measure (11 x 11 cells)", maxpixels=10^6, interpolate=FALSE)
+dev.off()
+
+r <- raster("c:/p/AB_data_v2015/out/figs/topo/vrm5x5.asc")
+png(file.path(ROOT, VER, "out", "figs", "topo", "vrm5x5.png"), width=600, height=1000)
+plot(r, axes=FALSE, box=FALSE, legend=TRUE, main="Vector ruggedness measure (5 x 5 cells)", maxpixels=10^6, interpolate=FALSE)
+dev.off()
+
+
 
 
 ## this is how detections can be mapped
