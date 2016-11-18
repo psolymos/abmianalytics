919155e94756ed9722956927b7f425ecfa0433e8
Peter Solymos <psolymos@gmail.com>
2016-11-17 17:05:17 -0700

    aru

--- aru/abmi/aru-opticut.R
+++ aru/abmi/aru-opticut.R
@@ -31,9 +31,6 @@ u1 <- uncertainty(o1, type="multi", B=99, cl=cl)
 u2 <- uncertainty(o2, type="multi", B=99, cl=cl)
 stopCluster(cl)
 
-save(o1, o1a, o2, u1, u2,
-    file="~/Dropbox/collaborations/opticut/R/abmi-aru-ocresults.Rdata")
-
 ## visualization
 
 ocoptions(theme=c("#7b3294", "#ffffbf", "#008837"))
@@ -47,3 +44,31 @@ pdf("aru-time.pdf", height=10, width=10)
 plot(o2,sort=1,mar=c(4,10,3,3),cut=2,ylab="",xlab="Time",show_I=FALSE, show_S=FALSE, cex.axis=0.6)
 dev.off()
 
+
+## week level clustering
+
+o3 <- opticut(xtv ~ 1,
+    data=x, strata=paste(x$Ssn, x$ToDc, sep=""), dist="binomial")
+o4 <- opticut(xtv ~ WIND + RAIN + NOISE + INDUSTRY,
+    data=x, strata=x$ToYc, dist="binomial")
+
+plot(o3, mar=c(4,10,3,3),cut=2,ylab="",xlab="Weeks", cex.axis=0.6)
+
+plot(o4,sort=1,mar=c(4,10,3,3),cut=2,ylab="",xlab="Weeks",
+    show_I=TRUE, show_S=TRUE, cex.axis=0.6, lower=0.25)
+bp <- summary(o4)$bestpart
+
+
+plot(hclust(vegan::vegdist(t(bp),"jaccard"), "ward.D2"))
+
+heatmap(bp, scale="none", col=occolors()(3)[2:3],
+    distfun=function(x) dist(x, "manhattan"))
+
+library(parallel)
+cl <- makeCluster(3)
+u3 <- uncertainty(o3, type="multi", B=99, cl=cl)
+u4 <- uncertainty(o4, type="multi", B=99, cl=cl)
+stopCluster(cl)
+
+save(o1, o1a, o2, u1, u2, o3, o4,# u4,
+    file="~/Dropbox/collaborations/opticut/R/abmi-aru-ocresults.Rdata")
