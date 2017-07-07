37727e33e3fca7b0655519a3fd6e44488650f8d7
Peter Solymos <psolymos@gmail.com>
2017-07-06 19:58:36 -0600

    bounded density

--- projects/10-yr/ch4-results.R
+++ projects/10-yr/ch4-results.R
@@ -448,20 +448,102 @@ rugged_mat <- function(x) {
 get_stuff <- function(TABLE, COL, north=TRUE)
     rugged_mat(lapply(structure(names(AllIn), names=names(AllIn)),
         get_stuff0, TABLE=TABLE, COL=COL, north=north))
-vp1 <- function(x, p=c(0, 1), ...) {
+bounded_density <- function(x, interval=c(-Inf, Inf), ...) {
+    require(intrval)
+    interval <- sort(interval[1L:2L])
+    if (!all(x %()% interval))
+        stop("found values of x outside of open interval")
+    a <- interval[1L]
+    b <- interval[2L]
+    bounds  <- is.finite(interval)
+    if (!bounds[1L] && !bounds[2L]) { # (Inf, Inf)
+        f <- finv <- function(x) x
+    }
+    if (bounds[1L] && !bounds[2L]) { # (a, Inf)
+        f <- function(x)
+            log(x-a)
+        finv <- function(z)
+            exp(z) + a
+    }
+    if (!bounds[1L] && bounds[2L]) { # (Inf, b)
+        f <- function(x)
+            log(b-x)
+        finv <- function(z)
+            b-exp(z)
+    }
+    if (bounds[1L] && bounds[2L]) { # (a, b)
+        f <- function(x)
+            qlogis((x-a) / (b-a))
+        finv <- function(z)
+            plogis(z) * (b-a) + a
+    }
+    fx <- f(x)
+    d <- density(fx, ...)
+    v <- d$x
+    dv <- diff(v)
+    h <- d$y
+    n <- length(h)
+    dh <- rowMeans(cbind(h[-n], h[-1L]))
+    A <- dv * dh
+    vinv <- finv(v)
+    hinv <- A / abs(diff(vinv))
+    hinv <- c(hinv[1L], hinv)
+    data.frame(x=vinv, y=hinv)
+}
+
+if (FALSE) {
+x <- rnorm(100)
+
+op <- par(mfrow=c(2,2))
+range(x)
+d1 <- bounded_density(exp(x))
+d2 <- bounded_density(exp(x), c(0, Inf))
+plot(bounded_density(x, type="l"))
+
+range(exp(x))
+d1 <- bounded_density(exp(x))
+d2 <- bounded_density(exp(x), c(0, Inf))
+plot(d1, type="l", ylim=c(range(d1$y, d2$y)))
+lines(d2, col=2)
+
+range(plogis(x)*4-1)
+d1 <- bounded_density(plogis(x)*4-1)
+d2 <- bounded_density(plogis(x)*4-1, c(-1, 3))
+plot(d1, type="l", ylim=c(range(d1$y, d2$y)))
+lines(d2, col=2)
+
+range(-exp(x)+3)
+d1 <- bounded_density(-exp(x)+3)
+d2 <- bounded_density(-exp(x)+3, c(-Inf, 3))
+plot(d1, type="l", ylim=c(range(d1$y, d2$y)))
+lines(d2, col=2)
+par(op)
+}
+
+cp1 <- function(x, p=c(0, 1), nmin=5, interval=c(-Inf, Inf), ...) {
     x2 <- x[!is.na(x)]
-    if (length(unique(x2)) < 5)
-        return(NULL)
     q <- quantile(x2, p)
     x2 <- x2[x2 > q[1] & x2 < q[2]]
-    d <- density(x2, ...)
-    data.frame(h=d$x, w=d$y/max(d$y))
+    if (length(unique(x2)) < nmin) {
+        m <- mean(x2)
+        s <- sd(x2)
+        v <- seq(min(min(x2), m-4*s), max(max(x2), m+4*s), length.out = 512)
+        v <- v %()% interval
+        d <- dnorm(v, m, s)
+        v <- c(v[1L], v, v[length(v)])
+        d <- c(0, d, 0)
+        out <- data.frame(h=v, w=d/max(d))
+    } else {
+        d <- bounded_density(x2, interval=interval, ...)
+        out <- data.frame(h=d$x, w=d$y/max(d$y))
+    }
+    out
 }
 vp <- function(x, p=c(0,1),
 col="#FF664D80", border="#FF664D",
 #col="#40E0D180", border="#40E0D1",
-ylim, ylab="", xlab="", main="", ...) {
-    xx <- apply(x, 2, vp1, p=p, ...)
+ylim, ylab="", xlab="", main="", nmin=5, interval=c(-Inf, Inf), ...) {
+    xx <- apply(x, 2, cp1, p=p, nmin=nmin, interval=interval, ...)
     if (missing(ylim))
         ylim <- range(x, na.rm=TRUE)
     plot(0, type="n", axes=FALSE, ann=FALSE, xlim=c(0.5, ncol(x)+0.5),
@@ -539,19 +621,25 @@ ustra <- get_stuff("sects", "UnitEffect.Transportation", FALSE)
 par(mfrow=c(3,2), las=1, yaxs="i")
 ylim <- c(-1, 1)
 p <- c(0, 1)
-vp(fu(uneng), main="Energy, North", ylim=ylim, p=p, ylab="Unit Effect")
+INT <- c(-1,1)
+vp(fu(uneng), main="Energy, North", ylim=ylim, p=p, ylab="Unit Effect",
+    interval=INT)
 abline(h=0, lty=2)
 abline(h=c(-0.5,0.5), lty=3, col="darkgrey")
-vp(fu(unagr), main="Algiculture, North", ylim=ylim, p=p, ylab="Unit Effect")
+vp(fu(unagr), main="Algiculture, North", ylim=ylim, p=p, ylab="Unit Effect",
+    interval=INT)
 abline(h=0, lty=2)
 abline(h=c(-0.5,0.5), lty=3, col="darkgrey")
-vp(fu(unurb), main="Rural/Urban, North", ylim=ylim, p=p, ylab="Unit Effect")
+vp(fu(unurb), main="Rural/Urban, North", ylim=ylim, p=p, ylab="Unit Effect",
+    interval=INT)
 abline(h=0, lty=2)
 abline(h=c(-0.5,0.5), lty=3, col="darkgrey")
-vp(fu(untra), main="Transportation, North", ylim=ylim, p=p, ylab="Unit Effect")
+vp(fu(untra), main="Transportation, North", ylim=ylim, p=p, ylab="Unit Effect",
+    interval=INT)
 abline(h=0, lty=2)
 abline(h=c(-0.5,0.5), lty=3, col="darkgrey")
-vp(fu(unfor), main="Forestry, North", ylim=ylim, p=p, ylab="Unit Effect")
+vp(fu(unfor), main="Forestry, North", ylim=ylim, p=p, ylab="Unit Effect",
+    interval=INT)
 abline(h=0, lty=2)
 abline(h=c(-0.5,0.5), lty=3, col="darkgrey")
 plot.new()
