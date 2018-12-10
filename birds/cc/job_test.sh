09f4f0f9b2703f09bdbdb5a1fbf3d410bea88c06
Peter Solymos <psolymos@gmail.com>
2018-12-10 10:48:51 -0700

    test runs

---
+++ birds/cc/job_test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --account=def-psolymos
+#SBATCH --ntasks=8
+#SBATCH --mem=2048M
+#SBATCH --time=00:30:00
+#SBATCH --job-name=test_makecluster
+#SBATCH --output=%x-%j.out
+#SBATCH --mail-user=solymos@ualberta.ca
+#SBATCH --mail-type=ALL
+
+# Load modules
+module nixpkgs/16.09
+module load gcc/7.3.0
+module load openmpi/3.1.2
+module load r/3.5.1
+
+# Export the nodes names.
+# If all processes are allocated on the same node,
+# NODESLIST contains : node1 node1 node1 node1
+export NODESLIST=$(echo $(srun hostname))
+
+# Run R script
+Rscript --vanilla test.R
---
+++ birds/cc/test.R
@@ -0,0 +1,92 @@
+## --- settings ---
+## file name for data bundle, need to be in /data/ dir
+fn <- "ab-birds-validation-2018-12-07.RData"
+## CAIC = alpha * AIC + (1 - alpha) * BIC, 1: AIC, 0: BIC
+CAICalpha <- 1
+## Number of bootstrap runs, 100 or 240
+MaxB <- 100
+## project name for storing the output
+PROJ <- "validation"
+## testing?
+
+cat("loading packages etc...")
+library(parallel)
+library(mefa4)
+library(opticut)
+source("~/repos/abmianalytics/birds/00-functions.R")
+
+
+## Create an array from the NODESLIST environnement variable
+if (interactive()) {
+    cat("OK\nNote: this is a test run...")
+    nodeslist <- 2
+    BBB <- 2
+    setwd("d:/abmi/AB_data_v2018/data/analysis/birds")
+} else {
+    cat("OK\ngetting nodes list...")
+    nodeslist <- unlist(strsplit(Sys.getenv("NODESLIST"), split=" "))
+    BBB <- MaxB
+    cat("OK\nNodes list:\n")
+    print(nodeslist)
+}
+
+## Create the cluster with the nodes name.
+## One process per count of node name.
+## nodeslist = node1 node1 node2 node2, means we are starting 2 processes
+## on node1, likewise on node2.
+cat("spawning workers...")
+cl <- makePSOCKcluster(nodeslist, type = "PSOCK")
+
+cat("OK\nload data on master...")
+load(file.path("data", fn))
+
+cat("OK\nload packages on workers...")
+tmpcl <- clusterEvalQ(cl, library(mefa4))
+tmpcl <- clusterEvalQ(cl, library(opticut))
+tmpcl <- clusterEvalQ(cl, source("~/repos/abmianalytics/birds/00-functions.R"))
+
+cat("OK\nexporting and data loading on workers...")
+tmpcl <- clusterExport(cl, "fn")
+if (interactive())
+    tmpcl <- clusterEvalQ(cl, setwd("d:/abmi/AB_data_v2018/data/analysis/birds"))
+#tmpcl <- clusterEvalQ(cl, load(file.path("data", fn)))
+clusterExport(cl, c("DAT", "YY", "OFF", "BB", "SSH"))
+
+cat("OK\nsetting checkpoint:\n")
+SPP <- colnames(YY)
+DONE <- character(0)
+if (interactive())
+    SPP <- SPP[1:2]
+
+SPP <- c("WTSP", "OVEN")
+
+DONE <- substr(list.files(paste0("out/", PROJ)), 1, 4)
+TOGO <- setdiff(SPP, DONE)
+
+cat("start running models:")
+while (length(TOGO) > 0) {
+    SPP1 <- TOGO[1L]
+    cat("\n-", length(DONE), "done,", length(TOGO), "more to go, doing", SPP1, "on", date(), "...")
+    if (interactive())
+        flush.console()
+    t0 <- proc.time()
+    z <- run_path1(1, "AMRO", mods, CAICalpha=1, wcol="vegw", ssh_class="vegc", ssh_fit="Space")
+    res <- parLapply(cl, 1:BBB, run_path1, i=SPP1, mods=mods, CAICalpha=CAICalpha,
+        wcol="vegw", ssh_class="vegc", ssh_fit="Space")
+    attr(res, "timing") <- proc.time() - t0
+    attr(res, "proj") <- PROJ
+    attr(res, "spp") <- SPP1
+    attr(res, "CAICalpha") <- CAICalpha
+    attr(res, "date") <- as.character(Sys.Date())
+    attr(res, "ncl") <- length(cl)
+    save(res,
+        file=paste0("out/", PROJ, "/", SPP1, ".RData"))
+    DONE <- substr(list.files(paste0("out/", PROJ)), 1, 4)
+    TOGO <- setdiff(SPP, DONE)
+    cat("OK")
+}
+
+## Releaseing resources.
+cat("\nshutting down.\n\nDONE!\n")
+stopCluster(cl)
+q("no")
