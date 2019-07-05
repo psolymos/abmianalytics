b76d5dcdc7ad362a9aba836bb6207fd2b28ce8bd
Peter Solymos <psolymos@gmail.com>
2019-07-05 12:57:19 -0600

    oven example contnd

--- projects/osm-oven/data/bayne-2016/condor.R
+++ projects/osm-oven/data/bayne-2016/condor.R
@@ -1,5 +1,5 @@
 library(intrval)
-x <- read.csv("_data/bayne-2016/condor-suppl.csv", stringsAsFactors=FALSE)
+x <- read.csv("data/bayne-2016/condor-suppl.csv", stringsAsFactors=FALSE)
 x1 <- x[,1:4]
 x1[] <- lapply(x1, as.factor)
 rownames(x1) <- x1$AOUcode
@@ -30,84 +30,29 @@ x3 <- do.call(cbind, lapply(1:ncol(x3), f2))
 rownames(x3) <- x1$AOUcode
 xx <- data.frame(x1, x2, x3)
 
-spp <- "ALFL"
+spp <- "OVEN"
 
 m <- matrix(x3[spp,], nrow=3)
 m0 <- matrix(m[1,], 3, 3)
 mL <- matrix(m[2,], 3, 3)
 mU <- matrix(m[3,], 3, 3)
-mSD <- (log(mU) - log(mL)) / qnorm(1-0.1/2)
-dimnames(m0) <- dimnames(mL) <- dimnames(mU) <-
-    dimnames(mSD) <- list(c(50, 100, Inf), c("S", "P", "W"))
+main <- paste(x1[spp, "CommonName"], x1[spp, "Habitat"])
+colnames(m0) <- c("Seismic", "Pipeline", "Wellpad")
+rownames(m0) <- c("50m", "100m", "Unlimited")
+dimnames(mL) <- dimnames(mU) <- dimnames(m0)
+m0[is.na(m0)] <- 0
+mL[is.na(mL)] <- 0
+mU[is.na(mU)] <- 0
+M <- list(est=m0, lwr=mL, upr=mU)
+dput(M)
 
-i <- "50"
-j <- "S"
 
-vals <- rnorm(10^6, log(m0[i,j]), mSD[i,j])
-plot(density(exp(vals)))
-
-col <- c('#66c2a5','#fc8d62','#e78ac3')
-
-pdf("spp.pdf", onefile=TRUE, height=5, width=7)
-for (spp in rownames(x1)) {
-    m <- matrix(x3[spp,], nrow=3)
-    m0 <- matrix(m[1,], 3, 3)
-    mL <- matrix(m[2,], 3, 3)
-    mU <- matrix(m[3,], 3, 3)
-    main <- paste(x1[spp, "CommonName"], x1[spp, "Habitat"])
-    colnames(m0) <- c("Seismic", "Pipeline", "Wellpad")
-    m0[is.na(m0)] <- 0
-    mL[is.na(mL)] <- 0
-    mU[is.na(mU)] <- 0
-    v <- barplot(m0,beside=TRUE, main=main, col=col, ylim=c(0, min(10, max(mU))))
-    abline(h=1, col='#8da0cb')
-    for (k in 1:9) {
-        int <- c(mL[k], mU[k])
-        lwd <- if (1 %[]% int) 1 else 3
-        lines(c(v[k], v[k]), int, lwd=lwd)
-    }
+v <- barplot(m0,beside=TRUE, main=main, col=col, ylim=c(0, min(10, max(mU))))
+abline(h=1, col='#8da0cb')
+for (k in 1:9) {
+    int <- c(mL[k], mU[k])
+    lwd <- if (1 %[]% int) 1 else 3
+    lines(c(v[k], v[k]), int, lwd=lwd)
 }
-dev.off()
-
-x3x <- x3[,!grepl("_", colnames(x3))]
-
-cc <- c(1,2,3,NA,4,5,6,NA,7,8,9)
-
-pdf("sppbox.pdf", onefile=TRUE, height=5, width=7)
-boxplot(x3x[,cc], ylim=c(0, 3), main="", col=c(col,NA), axes=FALSE)
-axis(2)
-axis(1,c(2,6,10),c("Seismic", "Pipeline", "Wellpad"), tick=F)
-abline(h=1, col='#8da0cb')
-boxplot(x3x[x1$Habitat!="OLD",cc], ylim=c(0, 3), main="Not OLD", col=c(col,NA), axes=FALSE)
-axis(2)
-axis(1,c(2,6,10),c("Seismic", "Pipeline", "Wellpad"), tick=F)
-abline(h=1, col='#8da0cb')
-for (i in c("OLD", "ALL", "SHR")) {
-    boxplot(x3x[x1$Habitat==i,cc], ylim=c(0, 3), main=i, col=c(col,NA), axes=FALSE,
-        ylab="Relative effect")
-    axis(2)
-    axis(1,c(2,6,10),c("Seismic", "Pipeline", "Wellpad"), tick=F)
-    abline(h=1, col='#8da0cb')
-    legend("topleft", fill=col, legend=c("50m", "100m", "Unlimited"), bty="n", title="Truncation distance")
-}
-dev.off()
-
-op <- par(mfrow=c(2,1))
-i <- "OLD"
-boxplot(x3x[x1$Habitat==i,cc], ylim=c(0, 3), main="Old forest species",
-    col=c(col,NA), axes=FALSE, ylab="Relative effect")
-axis(2)
-axis(1,c(2,6,10),c("Seismic", "Pipeline", "Wellpad"), tick=F)
-abline(h=1, col='#8da0cb')
-legend("topleft", fill=col, legend=c("50m", "100m", "Unlimited"), bty="n", title="Truncation distance")
-
-i <- "SHR"
-boxplot(x3x[x1$Habitat==i,cc], ylim=c(0, 3), main="Shrub species",
-    col=c(col,NA), axes=FALSE, ylab="Relative effect")
-axis(2)
-axis(1,c(2,6,10),c("Seismic", "Pipeline", "Wellpad"), tick=F)
-abline(h=1, col='#8da0cb')
-legend("topleft", fill=col, legend=c("50m", "100m", "Unlimited"), bty="n", title="Truncation distance")
-par(op)
 
 
--- projects/osm-oven/data/pred/mapping.R
+++ projects/osm-oven/data/pred/mapping.R
@@ -118,13 +118,21 @@ coordinates(xy) <- ~ POINT_X + POINT_Y
 proj4string(xy) <- "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
 xy <- spTransform(xy, proj4string(pl))
 o <- over(xy, pl)
-plot(xy, pch=".", col=ifelse(is.na(o$FIELDCODE), 1, 4))
+#plot(xy, pch=".", col=ifelse(is.na(o$FIELDCODE), 1, 4))
 
 ss <- !is.na(o$FIELDCODE)
 
+trVegSS <- trVeg[ss,]
+AVegSS <- colSums(trVegSS)
+
 resn <- load_species(file.path(ROOT, "out", PROJ, paste0(spp, ".RData")))
+
 ## north estimates
-ESTN <- suppressWarnings(get_coef(resn, Xn, stage="Space", na.out=FALSE))
+names(en$mods)
+STAGE <- "Space"
+#STAGE <- "HF"
+ESTN <- suppressWarnings(get_coef(resn, Xn, stage=STAGE, na.out=FALSE))
+
 b <- nrow(ESTN)
 CR <- matrix(0, sum(ss), b)
 rownames(CR) <- rownames(kgrid)[ss]
@@ -132,6 +140,42 @@ RF <- CR
 HABCR <- matrix(0, nrow(ch2veg), b)
 rownames(HABCR) <- rownames(ch2veg)
 HABRF <- HABCR
+
+## define SSH based on actuall partial backfill here based on trVeg
+
+## this goes into loop
+c1 <- colSums(abs(ESTN)) > 0
+if (any(c1[c("SSH_KM", "SSH05_KM")])) {
+    essh <- ESTN[,c("SSH_KM", "SSH05_KM")]
+#    c1[c("SSH_KM", "SSH05_KM")] <- FALSE # drop SSH
+#    mu <- X[,c1,drop=FALSE] %*% t(est[,c1,drop=FALSE])
+    mussh <- mu
+    mussh[] <- 0 # put SSH effects here
+    for (i in rownames(est)) {
+        ssh <- res[[as.integer(i)]]$ssh
+        v <- rowSums(SSH[,ssh$labels])
+        mussh[,i] <- essh[1,"SSH_KM"]*v + essh[1,"SSH05_KM"]*sqrt(v)
+    }
+    mu <- mu + mussh # add them up
+} else {
+    mu <- X[,c1,drop=FALSE] %*% t(est[,c1,drop=FALSE])
+}
+#mu
+
+lamMod <- matrix(NA, b, length(cfn$modif))
+colnames(lamMod) <- cfn$modif
+for (i in seq_len(b)) {
+        estn <- ESTN[i,]
+        if (estn["mSoft"] != 0 & estn["mEnSft"] == 0) {
+            estn["mEnSft"] <- estn["mSoft"]
+            estn["mTrSft"] <- estn["mSoft"]
+            estn["mSeism"] <- estn["mSoft"]
+        }
+        munMod <- structure(numeric(length(cfn$modif)), names=cfn$modif)
+        for (k in cfn$modif)
+            lamMod[i,k] <- linexp(1, estn[k], pm[k])
+}
+save(lamMod, ESTN, file="~/repos/abmianalytics/projects/osm-oven/data/pred/lamMod.RData")
 
 for (i in seq_len(b)) {
         if (i %% round(b/10) == 0) {
@@ -157,15 +201,15 @@ for (i in seq_len(b)) {
             EnSoftLin=unname(munMod["mEnSft"]),
             TrSoftLin=unname(munMod["mTrSft"]),
             Seismic=unname(munMod["mSeism"]))
-        munHab["Mine"] <- -1000
+        #munHab["Mine"] <- -1000
         ## expand coefficients for north
         prnCr <- munHab[match(ch2veg$cr2, names(munHab))]
         prnRf <- munHab[match(ch2veg$rf2, names(munHab))]
         prnCr[ch2veg$modif] <- prnRf[ch2veg$modif] + prnCr[ch2veg$modif]
         ## put pieces together for north
         ## multiplying with row normalized area gives the weighted averaging
-        ADnCr <- 100 * t(exp(prnCr) * t(trVeg[ss,])) * exp(munClim) # males / km cell
-        ADnRf <- 100 * t(exp(prnRf) * t(trVeg[ss,])) * exp(munClim)
+        ADnCr <- 100 * t(exp(prnCr) * t(trVegSS)) * exp(munClim) # males / km cell
+        ADnRf <- 100 * t(exp(prnRf) * t(trVegSS)) * exp(munClim)
         #ADnCrHab <- groupSums(ADnCr, 2, ch2veg$cr2)
 
         ## quantiles not applied -- look at that post hoc before summing up
@@ -174,7 +218,96 @@ for (i in seq_len(b)) {
         HABCR[colnames(ADnCr),i] <- colSums(ADnCr)
         HABRF[colnames(ADnRf),i] <- colSums(ADnRf)
 }
-save(HABRF, HABRF, HABRF, ch2veg,
+save(HABRF, HABCR, ch2veg, tv, AVegSS,
     file=paste0("d:/abmi/AB_data_v2018/data/analysis/birds/pred/oven/", spp, ".RData"))
 
+## sector effects
 
+load("d:/abmi/AB_data_v2018/data/analysis/birds/pred/oven/OVEN.RData")
+
+
+levs <- c(
+    "MineSite",
+    "Pipeline",
+    "TransmissionLine",
+    "RailHardSurface",
+    "RailVegetatedVerge",
+    "RoadHardSurface",
+    "RoadTrailVegetated",
+    "RoadVegetatedVerge",
+    "SeismicLineNarrow",
+    "SeismicLineWide",
+    "IndustrialSiteRural",
+    "UrbanIndustrial",
+    "WellSite")
+
+sect <- as.character(ch2veg$sector)
+sect[ch2veg$cr %in% levs] <- as.character(ch2veg$cr)[ch2veg$cr %in% levs]
+sect[sect %in% c("SeismicLineNarrow", "SeismicLineWide")] <- "SeismicLine"
+sect[sect %in% c("Pipeline", "TransmissionLine")] <- "PipeTransLine"
+sect[sect %in% c("RailHardSurface",
+    "RailVegetatedVerge",
+    "RoadHardSurface",
+    "RoadTrailVegetated",
+    "RoadVegetatedVerge")] <- "RoadRailVerge"
+sect[sect %in% c("IndustrialSiteRural", "UrbanIndustrial")] <- "Industrial"
+sect[sect == "Energy"] <- "OtherEnergy"
+data.frame(x=table(sect))
+
+HCR <- groupSums(HABCR, 1, sect)
+HRF <- groupSums(HABRF, 1, sect)
+A <- groupSums(matrix(AVegSS, ncol=1), 1, sect)
+
+save(HCR, HRF, A, file="~/repos/abmianalytics/projects/osm-oven/data/pred/osm-oven-space.RData")
+
+
+## mine site: take NDVI and use weighted average
+
+## NDVI was from 2017-2018.
+## Each pixel was the median value from June 1 - Aug 15th.
+## So it's safe to say the NDVI is from June/July either 2017 or 2018.
+## from Sentinel-2 10 m data, top of atmosphere corrected.
+#' Sentinel - NDVI (Normalized Difference Vegetation Index)
+#'
+#' This most known and used vegetation index is a simple, but effective VI for
+#' quantifying green vegetation. It normalizes green leaf scattering in the
+#' Near Infra-red wavelength and chlorophyll absorption in the red wavelength.
+#'
+#' Values description: The value range of an NDVI is -1 to 1.
+#' Negative values of NDVI (values approaching -1) correspond to water.
+#' Values close to zero (-0.1 to 0.1) generally correspond to barren areas of rock, sand, or snow.
+#' Low, positive values represent shrub and grassland (approximately 0.2 to 0.4),
+#' while high values indicate temperate and tropical rainforests (values approaching 1).
+
+
+
+
+#' This predicts with or without surrounding effects,
+#' output is `mu` matrix (PKEY x B) that is on log scale
+#' and has no offsets added to it
+predict_with_SSH <- function(res, X, SSH=NULL, stage=NULL) {
+    est <- suppressWarnings(get_coef(res, X, stage=stage, na.out=FALSE))
+#    OK <- !sapply(res, inherits, "try-error")
+#    ii <- sapply(res[OK], "[[", "iteration")
+#    notNA <- which(OK)
+#    est <- est[notNA,,drop=FALSE]
+    c1 <- colSums(abs(est)) > 0
+    if (any(c1[c("SSH_KM", "SSH05_KM")])) {
+        if (is.null(SSH))
+            stop("provide SSH")
+        essh <- est[,c("SSH_KM", "SSH05_KM")]
+        c1[c("SSH_KM", "SSH05_KM")] <- FALSE # drop SSH
+        mu <- X[,c1,drop=FALSE] %*% t(est[,c1,drop=FALSE])
+        mussh <- mu
+        mussh[] <- 0 # put SSH effects here
+        for (i in rownames(est)) {
+            ssh <- res[[as.integer(i)]]$ssh
+            v <- rowSums(SSH[,ssh$labels])
+            mussh[,i] <- essh[1,"SSH_KM"]*v + essh[1,"SSH05_KM"]*sqrt(v)
+        }
+        mu <- mu + mussh # add them up
+    } else {
+        mu <- X[,c1,drop=FALSE] %*% t(est[,c1,drop=FALSE])
+    }
+    mu
+}
