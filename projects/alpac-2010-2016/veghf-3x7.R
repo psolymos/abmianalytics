e5531aaf18e4a1bf31254e973cfcc53082e3013b
Peter Solymos <psolymos@gmail.com>
2020-01-13 12:40:45 -0700

    Update veghf-3x7.R

--- projects/alpac-2010-2016/veghf-3x7.R
+++ projects/alpac-2010-2016/veghf-3x7.R
@@ -5,16 +5,23 @@ meta <- read.csv("~/repos/abmianalytics/lookup/sitemetadata.csv")
 
 ROOT <- "d:/abmi/AB_data_v2019/data/raw/veghf/3x7"
 
-f <- file.path(ROOT, "20191219_SC_veghf3by7_2017.sqlite")
+#f <- file.path(ROOT, "20191219_SC_veghf3by7_2017.sqlite")
+f <- "s:/GC_eric/FromEric/IC_Reporting_Recovery/20200102_SC_veghf3by7_2017.sqlite"
 db <- dbConnect(RSQLite::SQLite(), f)
 lt <- dbListTables(db)
+lt
+
+## FMA
+
+
+d0 <- dbReadTable(db, "SC_FMA_Current_ALPAC_Vegetation_HFI2017_in3by7_rawdata")
+d0$Shape_Area <- d0$Area_m2
+#d1 <- dbReadTable(db, "SC_FMA_Current_ALPAC_Summary_HF_By_Category_unit_hectares")
 
 d <- dbReadTable(db, "SC_FMA_Current_ALPAC_Vegetation_VHF3by7_rawdata")
 yr <- sort(unique(d$year_3by7))
 d$Shape_Area <- d$Area_m2
 str(d)
-
-yr <- as.integer(substr(lt, nchar(lt)-3, nchar(lt)))
 
 for (yri in yr) {
     cat(yri, "\n")
@@ -32,6 +39,113 @@ for (yri in yr) {
     assign(paste0("dd_", yri), dd)
 }
 
+dd <- make_vegHF_wide_v6(d0,
+    col.label="ABMI_ID",
+    col.year=2017,
+    col.HFyear="YEAR",
+    col.HABIT="Vegetation",
+    col.SOIL="Soil_Type_1",
+    sparse=TRUE, HF_fine=TRUE) # use refined classes
+dd$scale <- "Backfill61 + 2017HFI in 3x7km"
+dx <- nonDuplicated(d, ABMI_ID, TRUE)[rownames(dd[[1]]),]
+dd <- fill_in_0ages_v6(dd, dx$NSRNAME, ages_list)
+dd_hfi2017 <- dd
 
-save(list=paste0("dd_", yr),
-    file="d:/abmi/AB_data_v2019/data/analysis/alpac/SC_FMA_Current_ALPAC_Vegetation_VHF3by7_veghfSummary.Rdata")
+dbDisconnect(db)
+
+save(list=c("dd_hfi2017", paste0("dd_", yr)),
+    file="d:/abmi/AB_data_v2019/data/analysis/alpac/SC_FMA_ALPAC_3by7_veghfSummary_20200102.RData")
+
+
+## AEI
+
+d0 <- dbReadTable(db, "SC_AEI_Alpac_Mistik_AB_only_Vegetation_HFI2017_in3by7_rawdata")
+d0$Shape_Area <- d0$Area_m2
+#d1 <- dbReadTable(db, "SC_AEI_Alpac_Mistik_AB_only_Summary_HF_By_Category_unit_hectares")
+
+d <- dbReadTable(db, "SC_AEI_Alpac_Mistik_AB_only_Vegetation_VHF3by7_rawdata")
+yr <- sort(unique(d$year_3by7))
+d$Shape_Area <- d$Area_m2
+str(d)
+
+for (yri in yr) {
+    cat(yri)
+    flush.console()
+    x <- d[d$year_3by7 == yri,]
+    issue <- which(x$FEATURE_TY %in% c("CUTBLOCK", "HARVEST-AREA") & is.na(x$YEAR))
+    if (length(issue)) {
+        cat("issue", yri)
+        x[issue, "YEAR"] <- round(mean(x$YEAR[x$FEATURE_TY %in% c("CUTBLOCK", "HARVEST-AREA") & !is.na(x$YEAR)]))
+    }
+    cat("\n")
+    dd <- make_vegHF_wide_v6(x,
+        col.label="ABMI_ID",
+        col.year=yri,
+        col.HFyear="YEAR",
+        col.HABIT="Vegetation",
+        col.SOIL="Soil_Type_1",
+        sparse=TRUE, HF_fine=TRUE) # use refined classes
+    dd$scale <- "Backfill61 + VerifiedHF in 3x7km"
+    dx <- nonDuplicated(d, ABMI_ID, TRUE)[rownames(dd[[1]]),]
+    dd <- fill_in_0ages_v6(dd, dx$NSRNAME, ages_list)
+    assign(paste0("dd_", yri), dd)
+}
+
+dd <- make_vegHF_wide_v6(d0,
+    col.label="ABMI_ID",
+    col.year=2017,
+    col.HFyear="YEAR",
+    col.HABIT="Vegetation",
+    col.SOIL="Soil_Type_1",
+    sparse=TRUE, HF_fine=TRUE) # use refined classes
+dd$scale <- "Backfill61 + 2017HFI in 3x7km"
+dx <- nonDuplicated(d, ABMI_ID, TRUE)[rownames(dd[[1]]),]
+dd <- fill_in_0ages_v6(dd, dx$NSRNAME, ages_list)
+dd_hfi2017 <- dd
+
+dbDisconnect(db)
+
+save(list=c("dd_hfi2017", paste0("dd_", yr)),
+    file="d:/abmi/AB_data_v2019/data/analysis/alpac/SC_AEI_Alpac_Mistik_AB_only_3by7_veghfSummary_20200102.RData")
+
+
+## Total area
+
+f <- "s:/GC_eric/FromEric/IC_Reporting_Recovery/20200110_SC_veghf2017.sqlite"
+db <- dbConnect(RSQLite::SQLite(), f)
+(lt <- dbListTables(db))
+
+d1 <- dbReadTable(db, "SC_FMA_Current_ALPAC_Vegetation_HFI2017_rawdata")
+d2 <- dbReadTable(db, "SC_AEI_Alpac_Mistik_AB_only_Vegetation_HFI2017_rawdata")
+
+d1$ID <- "ALL"
+d2$ID <- "ALL"
+dbDisconnect(db)
+
+dd <- make_vegHF_wide_v6(d1,
+    col.label="ID",
+    col.year=2017,
+    col.HFyear="YEAR",
+    col.HABIT="Vegetation",
+    col.SOIL="Soil_Type_1",
+    sparse=TRUE, HF_fine=TRUE) # use refined classes
+dd$scale <- "Backfill61 + 2017HFI in 3x7km"
+dd <- fill_in_0ages_v6(dd, "Central Mixedwood", ages_list)
+dd_fma <- dd
+
+dd <- make_vegHF_wide_v6(d2,
+    col.label="ID",
+    col.year=2017,
+    col.HFyear="YEAR",
+    col.HABIT="Vegetation",
+    col.SOIL="Soil_Type_1",
+    sparse=TRUE, HF_fine=TRUE) # use refined classes
+dd$scale <- "Backfill61 + 2017HFI in 3x7km"
+dd <- fill_in_0ages_v6(dd, "Central Mixedwood", ages_list)
+dd_aei <- dd
+
+
+save(dd_aei, dd_fma,
+    file="d:/abmi/AB_data_v2019/data/analysis/alpac/SC_AEIandFMA_veghfSummary_20200110.RData")
+
+
