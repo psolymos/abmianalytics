6177f8e21ae91d82d8217bed61564e0e57d7dec9
Peter Solymos <psolymos@gmail.com>
2020-04-07 13:58:53 -0600

    Update index.R

--- veghf/requests/2020-04-06-TWP/index.R
+++ veghf/requests/2020-04-06-TWP/index.R
@@ -18,31 +18,47 @@ vv$KM <- as.character(cure4insect:::.c4if$QT2KT[rownames(vv)])
 #z <- sort(unique(vv$KM))
 #all(ID==z)
 
+## IDs in the North
+IDN <- get_all_id("north")
+
 ## save results (spp x id) + metadata
 
 ST <- get_species_table(mregion="north")
 SPP <- rownames(ST)
 
-CR <- matrix(0, length(SPP), length(unique(vv$RT)))
-rownames(CR) <- SPP
-colnames(CR) <- sort(unique(vv$RT))
+## matrices to store quartiles
+CR1 <- matrix(0, length(SPP), length(unique(vv$RT)))
+rownames(CR1) <- SPP
+colnames(CR1) <- sort(unique(vv$RT))
+CR2 <- CR3 <- CR4 <- CR1
 
 #spp <- SPP[1]
 for (spp in SPP) {
     cat(spp, which(SPP==spp), "/", length(SPP), "\n")
     flush.console()
     y <- load_species_data(spp, boot=FALSE)
-    x <- rowSums(y$SA.Curr[vv$KM,])
-    x <- sum_by(x, vv$RT)
-    cr <- x[,"x"]/x[,"by"]
-    CR[spp, names(cr)] <- cr
+    Nkm <- rowSums(y$SA.Curr)
+    q <- quantile(Nkm, seq(0, 1, 0.25))
+    Qkm <- cut(Nkm, q, include.lowest=TRUE)
+    levels(Qkm) <- paste0("Q", 1:4)
+    names(Qkm) <- names(Nkm)
+    Qkm <- Qkm[vv$KM]
+    tab <- Xtab(~ RT + Qkm, vv)
+    tab <- 100 * tab/sum(tab)
+    CR1[spp, rownames(tab)] <- as.numeric(tab[,"Q1"])
+    CR2[spp, rownames(tab)] <- as.numeric(tab[,"Q2"])
+    CR3[spp, rownames(tab)] <- as.numeric(tab[,"Q3"])
+    CR4[spp, rownames(tab)] <- as.numeric(tab[,"Q4"])
 }
 
-range(CR)
+range(CR1)
+range(CR2)
+range(CR3)
+range(CR4)
 
-CR[which(apply(CR, 1, max) > 2),]
-M <- apply(CR, 1, max)
-out <- data.frame(ST[,c(1:5, 11)], round(CR, 6))[M > 0.00001 & M < 2,]
+out <- data.frame(ST[,c(1:5, 11)],
+    Q1=round(CR1, 2),Q2=round(CR2, 2),Q3=round(CR3, 2),Q4=round(CR4, 2))
+
 write.csv(out, row.names=FALSE,
-    file="GRE-9-10-TWP-102-103_ABMI-species_2020-04-06.csv")
+    file="GRE-9-10-TWP-102-103_ABMI-species_2020-04-07.csv")
 
